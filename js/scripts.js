c08f1bb3aa980e2e884ae7c1fb04ed271afbb545
Nayanjeet Saikia <65288687+Neyen108@users.noreply.github.com>
2020-11-26 02:05:38 +0530

    added changes to images and detail function

--- js/scripts.js
+++ js/scripts.js
@@ -64,9 +64,178 @@ Favs = {
     });
   },
 
+  //----------------------------------------------------------
+
+  //randomExchange() function: Exchanges backround images for 2 random blocks
+
+  randomExchange: function() {
+
+    let $elem1, $elem2, 
+      elem1_url, elem2_url,
+      elem1_ranking, elem2_ranking;
+
+    do{
+      $elem1 = $('.js-album-leaf').random();
+      $elem2 = $('.js-album-leaf').random();
+      elem1_url = $elem1.css('background-image');
+      elem2_url = $elem2.css('background-image');
+      elem1_ranking = $elem1.attr('data-ranking');
+      elem2_ranking = $elem2.attr('data-ranking');
+    }while(elem1_url == elem2_url);    //prevents choosing the same album leafs
+
+    $elem1.addClass('during-exchange');
+    $elem2.addClass('during-exchange');
+
+    setTimeout(function() {
+      // switch background colors, data ranking, and remove 'during-exchnage' class
+
+      $elem1
+        .css('background-image', elem2_url)
+        .attr('data-ranking', elem2_ranking)
+        .removeClass('during-exchange');
+
+      $elem2
+        .css('background-image', elem1_url)
+        .attr('data-ranking', elem1_ranking)
+        .removeClass('during-exchange');
+
+      $('.js-album-leaf.hover').mouseenter();   //retrigger events that should happen when mouse is over a leaf and the image just changed
+
+    }, 450);
+
+  },
+
+  //randomExchangeStart()
+
+  randomExchangeStart : function() {
+
+    if( ! $('body').hasClass('album-detail')) {
+
+      clearInterval(s.randomExchangeTimer);
+
+      s.randomExchangeTimer = setTimeout(function() {
+
+        //do an exchange of album leaves
+        Favs.randomExchange();
+
+        //increase timeout interval
+        s.randomExchangeInterval += s.randomExchangeIntervalIncrease;
+
+        if(s.randomExchangeInterval > s.randomExchangeIntervalMax) {
+          s.randomExchangeInterval = s.randomExchangeIntervalMax;
+        }
+
+
+        //call timeout again
+
+        Favs.randomExchangeStart();
+
+      }, s.randomExchangeInterval);
+
+    }
+  },
+
+  //randomExchangeStop function to stop the exchnage
+
+  randomExchangeStop : function() {
+
+    clearInterval(s.randomExchangeTimer);
+    
+  },
+
+  // enterDetailView finction: shows detail view of the album
+
+  enterDetailView: function(e) {
+
+    let $this = $(e);
+
+    // Change background to current album image
+    const albumSlug = $this.data('album-slug');
+    $('.js-albums').css('backgorund-image', 'url(images/' + albumSlug + '.jpg');
+
+
+    //get album data
+    const artist = $this.find('.js-list-artist').text();
+    const title = $this.data('title');
+    const number = $('.js-list-item').index($this) + 1;
+
+    //output meta data
+    $('.js-artist').text(artist);
+    $('.js-title').text(title);
+    $('.js-number').text(number);
+
+    //show detail by adding the album-detail class
+    $('body').addClass('album-detail');
+
+    //stop the random exchnage
+    Favs.randomExchangeStop();
+
+    //Audio
+    if(s.audioOn) {
+
+      if(s.howl) {
+        s.howl.stop(); //just in case
+      }
+
+
+      //get audio info for current selection
+      let audioSrc = [
+        'audio/' + albumSlug + '.mp3',
+      ];
+
+
+      //set up howl
+      s.howl = new Howl({
+        src: audioSrc,
+        html5: true,
+      });
+
+      //play and fade in 
+      let howlInstance = s.howl.play();
+      s.howl.fade(0, s.audioMaxVolume, a.audioFadeIn, howlInstance);
+
+      //save what is playing in variable
+      s.audioPlaying = howlInstance;
+
+      //cancel any audio timers thats already playing
+      clearTimeout(s.audioPlayTimer);
+      clearTimeout(s.audioStopTimer);
+
+      //play audio clip until a set max length
+      s.audioPlayTimer = setTimeout(function(){
+
+        //Fade audio out
+        s.howl.fade(s.audioMaxVolume, 0, s.audioFadeOut, s.audioPlaying);
+        s.audioPlaying = false;
+
+        //Exit detail view
+        Favs.exitDetailView();
+
+      }, s.audioMaxLength);
+
+      //progress bar
+      Favs.progressBar('start');
+    }
+
+  },
+
+  
+  
+
+
+
+
+
+
+
 
 
 }
+
+
+
+
+//start the script
 
 $( document ).ready(function() {
 	Favs.init();
